acbee4b1d13a3666d5d6b0fddde11036548c4ebb
Tharindu209 <tharindudamruwan23@gmail.com>
2024-03-04 22:49:07 +0530

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello TS");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(compression());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("Server running on http://localhost:8080/");
+})
